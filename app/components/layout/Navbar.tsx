4a2db5731a182abb336b1607c41c088f19cb58aa
Phearun123 <you@example.com>
2024-05-17 11:32:45 +0700

    fix style navbar

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -12,8 +12,8 @@ const Navbar = () => {
 
     return (
         <>
-            <div id="ks_wt_app_header"
-                 className="ks_d_flex justify-content-end">
+            <div id="ks_wt_app_default"
+                 className="ks_d_flex justify-content-end ks_brd_btm ks_pd_20">
 
                 <div
                     id="ks_wt_app_header_action_icon"
