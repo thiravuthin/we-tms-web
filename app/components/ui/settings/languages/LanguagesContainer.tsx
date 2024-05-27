02376779db0c5a0b4fb811953fbfd782d26625aa
vuthin <thiravuthin@gmail.com>
2024-05-27 14:19:57 +0700

    fix language

--- app/components/ui/settings/languages/LanguagesContainer.tsx
+++ app/components/ui/settings/languages/LanguagesContainer.tsx
@@ -1,26 +1,12 @@
 'use client'
 
-import {useState} from "react";
 import LanguageList from "@/app/components/ui/settings/languages/LanguageList";
 
-interface Language {
-    name: string;
-    abbreviation: string;
-    date: string;
-}
 
 const LanguagesContainer = () => {
-
-    const [languages, setLanguages] = useState<Language[]>([]);
-
-    const handleSaveLanguage = (newLanguage: Omit<Language, 'date'>) => {
-        const newLanguageWithDate = { ...newLanguage, date: new Date().toLocaleString() };
-        setLanguages([...languages, newLanguageWithDate]);
-    };
-
     return (
         <div className=" ks-wt-modal-wrapper ">
-            <LanguageList onSaveLanguage={handleSaveLanguage} />
+            <LanguageList />
         </div>
     );
 };
