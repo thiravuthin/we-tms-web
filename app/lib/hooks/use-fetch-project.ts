c4525ea83e1910b0663b7894c08fcfcbf408677e
Phearun123 <you@example.com>
2024-05-30 16:05:26 +0700

    fixed project scrolling

--- app/lib/hooks/use-fetch-project.ts
+++ app/lib/hooks/use-fetch-project.ts
@@ -1,9 +1,10 @@
-import {useQuery} from "@tanstack/react-query";
+import {ProjectManagements, ProjectParams} from "@/app/lib/types/project";
+import {useInfiniteQuery} from "@tanstack/react-query";
 import {projectService} from "@/service/project.service";
 import {useSearchParams} from "next/navigation";
 
 
-const useFetchProject = () => {
+const useFetchProjects = () => {
     const params = useSearchParams();
     const pageSize = params.get("page_size") || 10;
     let requestParams = {
@@ -12,15 +13,35 @@ const useFetchProject = () => {
         search_value: params.get("search_value") || "",
         sort_columns: params.get("sort_columns") || "",
     }
-    const projectQuery = useQuery({
+    const {
+        data,
+        isFetchingNextPage,
+        isFetchingPreviousPage,
+        fetchNextPage,
+        fetchPreviousPage,
+        hasNextPage,
+        hasPreviousPage,
+        isError,
+        error
+    } = useInfiniteQuery<ProjectManagements, Error>({
         queryKey: ['projects', requestParams],
-        queryFn: () => projectService.getProjects(requestParams)
-    })
+        queryFn: ({ pageParam = 0 }) => projectService.getProjects({ ...requestParams, page_number: pageParam }),
+        initialPageParam: 0,
+        getNextPageParam: (lastPage) => lastPage.pagination.current_page < lastPage.pagination.total_pages ? lastPage.pagination.current_page + 1 : undefined,
+        getPreviousPageParam: (firstPage) => firstPage.pagination.current_page > 0 ? firstPage.pagination.current_page - 1 : undefined,
+    });
 
     return {
-        data: projectQuery?.data?.data ?? [],
-        pagination: projectQuery?.data?.pagination
+        data,
+        isFetchingNextPage,
+        isFetchingPreviousPage,
+        fetchNextPage,
+        fetchPreviousPage,
+        hasNextPage,
+        hasPreviousPage,
+        isError,
+        error
     };
-}
+};
 
-export default useFetchProject
+export default useFetchProjects
